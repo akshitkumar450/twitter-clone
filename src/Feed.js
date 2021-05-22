64aa1f8f94a8b702ebef5ebb9684fb30744f17e9
Akshit Kumar <akshitkumar450@gmail.com>
2021-05-23 05:26:08 +0530

    header

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import './Feed.css'
 
 function Feed() {
     return (
-        <div>
-            feed
+        <div className='feed'>
             {/**Header */}
+            <div className='feed__header'>
+                <h2>HOME</h2>
+            </div>
+
             {/**tweetBox */}
+
             {/**posts */}
             {/**posts */}
             {/**posts */}
